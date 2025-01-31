3f7df8581b17b76bec32f129950688dab151c2cd
xiomarasabi <xiomarasabi2003@gmail.com>
2025-01-30 22:38:53 -0500

    Se realizo lo necesario para el funcionamiento de un crud para el registro y manejo del PEA(plagas/enfermedades/arvence)

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import bodyParser from "body-parser";
 import express from "express";
 import routerPlantacion from "./src/router/router.plantacion.js";
 import routerCultivo from "./src/router/router.cultivo.js";
+import routerPea from "./src/router/router.pea.js";
 
 
 const app = express();
@@ -10,6 +11,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(routerPlantacion);
 app.use(routerCultivo);
+app.use(routerPea);
 
 
 app.listen(3000, () => {
---
+++ src/controller/controller.pea.js
@@ -0,0 +1,98 @@
+import {pool} from "../config/conexion.js"
+
+export const postPea = async (req, res) => {
+    try {
+        const { nombre, descripcion } = req.body;
+        const sql = "INSERT INTO pea(nombre, descripcion) VALUES($1, $2)";
+        const rows = await pool.query(sql, [nombre, descripcion]);
+
+        if (rows.rowCount > 0) {
+            return res.status(200).json({ "message": "PEA registrado correctamente" });
+        } else {
+            return res.status(404).json({ "message": "No se pudo registrar el PEA" });
+        }
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ "message": "Error en el servidor" });
+    }
+};
+
+export const getPea = async (req, res) => {
+    try {
+
+        const sql = `SELECT 
+                    id_pea, 
+                    nombre, 
+                    descripcion 
+                FROM pea`;
+
+        const result = await pool.query(sql);
+
+        if (result.rows.length > 0) {
+            const peas = result.rows.map(pea => ({
+                id_pea: pea.id_pea,
+                nombre: pea.nombre,
+                descripcion: pea.descripcion
+            }));
+
+            res.status(200).json({ peas });
+        } else {
+            res.status(404).json({ msg: 'No hay PEA registrados' });
+        }
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ msg: 'Error en el servidor' });
+    }
+};
+
+export const actualizarPea= async (req, res) => {
+    try{
+        const { nombre, descripcion } = req.body;
+        const id = req.params.id_pea;
+
+        const sql = `UPDATE pea SET nombre = $1, descripcion = $2 WHERE id_pea = $3`;
+        const { rowCount } = await pool.query(sql, [nombre, descripcion, id]);
+        if (rowCount > 0) {
+            return res.status(200).json({ "message": "PEA editada correctamente." });
+        } else {
+            return res.status(404).json({ "message": "No se pudo editar la PEA." });
+        }
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ "message": "Error en el servidor." });
+    }
+    
+};
+export const IdPea = async (req, res) => {
+    try {
+        const { id_pea } = req.params;
+
+        console.log("Buscando PEA con ID:", id_pea);
+
+        const sql = `SELECT 
+                    id_pea, 
+                    nombre, 
+                    descripcion 
+                FROM pea 
+                WHERE id_pea = $1`;
+
+        const result = await pool.query(sql, [id_pea]);
+
+        if (result.rows.length > 0) {
+            const pea = result.rows.map(p => ({
+                id_pea: p.id_pea,
+                nombre: p.nombre,
+                descripcion: p.descripcion
+            }));
+
+            res.status(200).json({ pea });
+        } else {
+            res.status(404).json({ msg: 'No se encontró el PEA' });
+        }
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ "message": "Error en el servidor." });
+    }
+};
