576b5bf2ec6419670a89992399fed1d32dda2c27
wilson-rodriguez09 <samboniwilson09@gmail.com>
2025-02-06 14:34:30 -0500

    Documentacion modulo usuarios utilizando swagger-jsdoc y swagger-ui-express

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import bodyParser from 'body-parser';
 import routerRol from './src/routers/usuarios/router.rol.js';
 import routerUsuarios from './src/routers/usuarios/router.usuarios.js';
 import router from './src/routers/usuarios/router.autenticacion.js';
+import swaggerUI from 'swagger-ui-express';
+import swaggerSpec from './src/views/swagger.js';
 
 
 const app = express();
@@ -13,7 +15,12 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(routerRol)
 app.use(routerUsuarios)
 app.use(router)
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerSpec));
 
 app.listen(3000,()=>{
     console.log("servidor iniciado en el puerto 3000")
+    
+    console.log(
+        `Version 1 de documentacion dsiponible en url http://localhost:3000/api-docs`
+    )
 });
